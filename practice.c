94b2f1a608badee490d0e94f491a05068aea6b7e
Kirtisinh Zala <kirtisinhzala711@gmail.com>
2026-05-17 08:59:07 +0530

    Learning Operators

--- practice.c
+++ practice.c
@@ -39,16 +39,45 @@ int main() {
   scanf("%f", &radius);
 
   printf("Area of circle is : %f \n", pi * radius * radius);
-*/
+
 
 // Q. Solve int a = 1.999999 and tell whats output here a=1 or a=2
     int a = (int) 1.999999;
     printf("%d \n", a); //output : 1
 
-    // Q. Opertator precedence & Associativity Rule
+ // Q. Opertator precedence & Associativity Rule
 int k = 5*2-2*3;  //1)5*2/2*3  2)5*(2/2)*3  3)5+2/2*3
 printf("%d", k);
     
+
+
+//OPERATORS -> Relation Operator
+printf("%d \n", 5 > 3);
+printf("%d \n", 5 >= 3);
+printf("%d \n", 5 < 3);
+printf("%d \n", 5 <= 3);
+printf("%d \n", 5 == 3);
+printf("%d \n", 5 != 3);
+
+//Logical Operator
+printf("%d \n", 4 > 2 && 7 < 10);
+printf("%d \n", 55 == 44 && 101 != 101);
+printf("%d \n", 5 == 5 || 40 <= 40);
+printf("%d \n", !(5 != 10));
+
+*/
+
+//Assignment Operator 
+int x,y,z,v,w;
+int k,l,m,n,o;
+x = y = z = v = w = 10;
+k = l = m = n = o = 5;
+
+printf("%d \n", x += k);
+printf("%d \n", y -= l);  
+printf("%d \n", z *= m);
+printf("%d \n", v /= n);
+printf("%d \n", w %= o);
 
     return 0;
 }
