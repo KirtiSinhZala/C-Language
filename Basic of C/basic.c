95f6d66d9a10a463dae42f86b4e6685f26c982d6
Kirtisinh Zala <kirtisinhzala711@gmail.com>
2026-05-18 08:11:37 +0530

    Added Basic Concept folder

---
+++ Basic of C/basic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+
+     int age = 21;
+   // float pi = 3.14;
+    char star = '*';
+
+    printf("age is %d \n", age);
+  //  printf("pi value is %f \n", pi);
+    printf("star looks like  %c \n", star);
+
+    int a,b;
+    printf("enter value of a : ");
+    scanf("%d", &a);
+
+      printf("enter value of b : ");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("sum of a and b is %d \n",sum);
+    // printf("sum of a and b is %d", a + b);  operation without third variable
+
+
+
+    return 0;
+}
