ac12071474354d7c849e1da905f07a4545b7062c
Kirtisinh Zala <kirtisinhzala711@gmail.com>
2026-05-24 18:50:34 +0530

    Just started Arrays

---
+++ Arrays/array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    int marks1 = 91;
+    int marks2 = 95;
+    int marks3 = 99;
+    
+    //with use of Array
+    int marks[] = {97,98,89};
+    return 0;
+}
---
+++ Arrays/question.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int countOdd(int arr[], int n);
+int main() {
+
+//Q. write a function to count the number of odd numbers in an array.
+ //   int arr[] = {1,2,3,4,5};
+ //   printf("%d \n",countOdd(arr,6));
+ 
+//for the given array, what will the following give?  a. *(arr+2)  b. *(arr+5)
+    int arr[] = {1,2,3,4,5};
+    printf("%d \n",*(arr+2));
+    printf("%d \n",*(arr+5));
+    return 0;
+}
+
+int countOdd(int arr[], int n){
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i] % 2 != 0){
+            count++;
+        }
+    }
+    return count;
+}
