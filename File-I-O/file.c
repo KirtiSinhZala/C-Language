b30c1046ff491bf4018b4068c79f7b4659e2c3aa
Kirtisinh Zala <kirtisinhzala711@gmail.com>
2026-05-28 23:54:11 +0530

    basic functions of FILE I/O

--- File-I-O/file.c
+++ File-I-O/file.c
@@ -4,22 +4,52 @@ int main(){
 
 // Q.  Check if a file exists before reading form it.
     FILE *fptr;
-    fptr = fopen("test.txt", "r");
-    if(fptr == NULL){
-        printf("file does not exists!");
-    }else {
-        printf("file Accessed");
+    FILE *fptr2;
+    FILE *fptr3;
+     fptr = fopen("test.txt", "w");
+     fptr2 = fopen("fruit.txt", "w");
+     fptr3 = fopen("text.txt", "r");
+ /*   // if(fptr == NULL){
+    //     printf("file does not exists!");
+    // }else {
+    //     printf("file Accessed");
+    // }
+    
+
+
+    
+    fprintf(fptr2, "%c", 'M');
+    fprintf(fptr2, "%c", 'A');
+    fprintf(fptr2, "%c", 'N');
+    fprintf(fptr2, "%c", 'G');
+    fprintf(fptr2, "%c", 'O');
+    
+
+//Read & Write a character
+    printf("%c \n", fgetc(fptr2));
+    printf("%c \n", fgetc(fptr2));
+    printf("%c \n", fgetc(fptr2));
+    printf("%c \n", fgetc(fptr2));
+    printf("%c \n", fgetc(fptr2));
+
+
+    fputc('A', fptr);
+    fputc('P', fptr);
+    fputc('p', fptr);
+    fputc('L', fptr);
+    fputc('E', fptr);
+  */
+    char ch;
+    ch = fgetc(fptr3);
+    while(ch != EOF){
+        printf("%c", ch);
+        ch = fgetc(fptr3);
     }
-    fclose(fptr);
+    printf("\n");
 
 
-
-
- 
-
-
-
-
-
+fclose(fptr3);
+fclose(fptr2);
+fclose(fptr);
     return 0;
 }
